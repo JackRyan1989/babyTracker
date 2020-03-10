56095fa66b4938d938b81dafe98d170d8b8ad0ec
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-09 22:34:30 -0400

    Working on login.

--- baby-tracker-app/src/pages/login.js
+++ baby-tracker-app/src/pages/login.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import { Avatar, Button, Container, TextField, Typography, CssBaseline, Paper } from '@material-ui/core/';
 import { makeStyles } from '@material-ui/core/styles';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
@@ -44,6 +45,17 @@ const useStyles = makeStyles(theme => ({
         textAlign: 'center',
         alignContent: 'center',
     },
+    errorHeader: {
+        width: '40%',
+        color: 'white',
+        backgroundColor: '#f28c82',
+        textAlign: 'center',
+        alignContent: 'center',
+        border: 'none',
+        borderRadius: '5px',
+        margin: '0 25%',
+        padding: theme.spacing(1),
+    },
     form: {
         width: '100%', // Fix IE 11 issue.
         marginTop: theme.spacing(1),
@@ -55,19 +67,22 @@ function Login() {
     const [app, setApp] = useState('');
     const [db, setDB] = useState('');
     const [loginError, setError] = useState('');
-    const classes = useStyles();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+    const classes = useStyles();
 
     //Setup Mongo Stitch App:
     useEffect(() => {
-        const client = Stitch.initializeDefaultAppClient("baldytracker-slqit");
+        const client = Stitch.initializeDefaultAppClient("baldytracker-vlawr");
         const mongodb = client.getServiceClient(
             RemoteMongoClient.factory,
             "mongodb-atlas"
         );
-        setDB(mongodb.db('babytracker'));
-        setApp(Stitch.defaultAppClient);
+        const db = mongodb.db('baldyData');
+        setDB(db);
+        const app = Stitch.defaultAppClient;
+        setApp(app);
     }, []);
 
     //Functions:
@@ -77,12 +92,13 @@ function Login() {
     const passwordInput = event => {
         setPassword(event.target.value);
     };
-    const login = event => {
+    const login = () => {
+        console.log(email, password);
         const credential = new UserPasswordCredential(email, password);
-        console.log(credential);
         app.auth.loginWithCredential(credential)
             .then(authedUser => {
                 console.log(`successfully logged in with id: ${authedUser.id}`)
+                setRedirect(true);
             })
             .catch(err => {
                 console.error(`login failed with error: ${err}`)
@@ -90,56 +106,62 @@ function Login() {
             })
     }
 
-    return (
-        <Container maxWidth='lg'>
-            <CssBaseline />
-            <Paper className={classes.box}>
-                <Avatar className={classes.avatar}>
-                    <LockOutlinedIcon />
-                </Avatar>
-                <Typography className={classes.loginHeader} variant='h2'>Sign In</Typography>
-                <form className={classes.form} noValidate>
-                    <TextField
-                        className={classes.text}
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        id="email"
-                        label="Email Address"
-                        name="email"
-                        autoComplete="email"
-                        value={email}
-                        onChange={emailInput}
-                        autoFocus
-                    />
-                    <TextField
-                        className={classes.text}
-                        variant="outlined"
-                        margin="normal"
-                        required
-                        fullWidth
-                        name="password"
-                        label="Password"
-                        value={password}
-                        onChange={passwordInput}
-                        type="password"
-                        id="password"
-                        autoComplete="current-password"
-                    />
-                </form>
-                <Button
-                    type="submit"
-                    variant="contained"
-                    color="primary"
-                    className={classes.loginButton}
-                    onClick={login}
-                >
-                    Sign In
+    if (redirect) {
+        return <Typography>FART</Typography>
+        // return <Redirect to={{ pathname: '/add', state: { test: 'test' } }} />
+    } else {
+        return (
+            <Container maxWidth='lg'>
+                <CssBaseline />
+                <Paper className={classes.box}>
+                    <Avatar className={classes.avatar}>
+                        <LockOutlinedIcon />
+                    </Avatar>
+                    <Typography className={classes.loginHeader} variant='h2'>Sign In</Typography>
+                    <form className={classes.form} noValidate>
+                        <TextField
+                            className={classes.text}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            value={email}
+                            onChange={emailInput}
+                            autoFocus
+                        />
+                        <TextField
+                            className={classes.text}
+                            variant="outlined"
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="password"
+                            label="Password"
+                            value={password}
+                            onChange={passwordInput}
+                            type="password"
+                            id="password"
+                            autoComplete="current-password"
+                        />
+                    </form>
+                    {loginError ? <Typography className={classes.errorHeader}>Invalid Credentials</Typography> : null}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className={classes.loginButton}
+                        onClick={login}
+                    >
+                        Sign In
                 </Button>
-            </Paper>
-        </Container>
-    )
+                </Paper>
+            </Container>
+        )
+    }
 }
 
 export default Login;
