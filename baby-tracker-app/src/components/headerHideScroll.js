c0e0cb2baf64f69245a67c872621dce1ae15c648
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-12 21:21:29 -0400

    Slowly styling and building out the UI of the Add Data page.

--- baby-tracker-app/src/components/headerHideScroll.js
+++ baby-tracker-app/src/components/headerHideScroll.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Typography from '@material-ui/core/Typography';
 import AppBar from '@material-ui/core/AppBar';
 import PropTypes from 'prop-types';
 import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
 import useScrollTrigger from '@material-ui/core/useScrollTrigger';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import { withStyles } from '@material-ui/core/styles';
 
+const StyledMenu = withStyles({
+    paper: {
+      border: '1px solid #d3d4d5',
+    },
+  })(props => (
+    <Menu
+      elevation={0}
+      getContentAnchorEl={null}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'center',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'center',
+      }}
+      {...props}
+    />
+  ));
 
 function ElevationScroll(props) {
     const { children } = props;
@@ -24,10 +49,33 @@ ElevationScroll.propTypes = {
 };
 
 export default function HeaderHideScroll(props) {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const handleClick = event => {
+        console.log('clicked');
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
     return (
         <ElevationScroll {...props}>
             <AppBar>
                 <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <MenuIcon onClick={handleClick} />
+                            <StyledMenu
+                                id="simple-menu"
+                                anchorEl={anchorEl}
+                                keepMounted
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                                <MenuItem onClick={handleClose}>My account</MenuItem>
+                                <MenuItem onClick={handleClose}>Logout</MenuItem>
+                            </StyledMenu>
+                    </IconButton>
                     <Typography variant="h5" component="div">BabyTracker App</Typography>
                 </Toolbar>
             </AppBar>
--- baby-tracker-app/src/pages/addData.js
+++ baby-tracker-app/src/pages/addData.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import { Avatar, Button, Container, TextField, Typography, CssBaseline, Paper } from '@material-ui/core/';
+import HeaderHideScroll from '../components/headerHideScroll';
 
 function AddData(props) {
 
     return (
+        <>
+        <HeaderHideScroll></HeaderHideScroll>
         <Typography>HI!</Typography>
+        </>
     )
 
 };
