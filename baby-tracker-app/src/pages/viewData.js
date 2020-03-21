47b2ca1cc5db73d01398852f5fe173405ce06f1d
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-20 21:23:25 -0400

    Added some styling for an effort display.

--- baby-tracker-app/src/pages/viewData.js
+++ baby-tracker-app/src/pages/viewData.js
@@ -1,10 +1,34 @@
 import React from 'react';
 import HeaderHideScroll from '../components/headerHideScroll';
+import DisplayBurden from '../components/displayBurden';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+    paper: {
+        margin: '15% 5% 5% 5%',
+        padding: theme.spacing(2),
+        backgroundColor: '#eceff1',
+        padding: '5px',
+        border: 'none',
+        borderRadius: '2px',
+    },
+})
+);
 
 export default function ViewData(props) {
+    const classes = useStyles();
     return (
         <>
         <HeaderHideScroll {...props}></HeaderHideScroll>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={6} lg={6}><DisplayBurden/></Grid>
+            <Grid item xs={12} sm={12} md={6} lg={6}><Paper className={classes.paper}></Paper></Grid>
+        </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12} sm={12} md={12} lg={12}><Paper className={classes.paper}>Placeholder</Paper></Grid>
+        </Grid>
         </>
     )
 };
