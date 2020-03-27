2a8712e243118ef40a590cf0bb38e41df49ade59
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-27 09:25:00 -0400

    Updates

---
+++ precache-manifest.49fe1e74fbe36a95fb3425d9b6a90723.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ea1585c41fe1db0eb3ddb8a2f3f25d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3a8e559e7a443d5ed6c6",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f0b075b60d623891717e",
+    "url": "/static/js/2.6a97e7f9.chunk.js"
+  },
+  {
+    "revision": "a3fb8a051b154c089c5dba8c6d16a92d",
+    "url": "/static/js/2.6a97e7f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a8e559e7a443d5ed6c6",
+    "url": "/static/js/main.9a3dbe9a.chunk.js"
+  },
+  {
+    "revision": "f0c921927534ddbe6be8",
+    "url": "/static/js/runtime-main.087b51b4.js"
+  }
+]);
