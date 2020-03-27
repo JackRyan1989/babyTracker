fe75fbd32d9609a3f15105d4472a4ad2609682ea
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-27 09:31:24 -0400

    Updates

---
+++ precache-manifest.ee460bd7aab795baa7d06dd829fd2422.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "541313976a3a68c41f2635da908cf27c",
+    "url": "/babyTracker/index.html"
+  },
+  {
+    "revision": "ec61c85e423fe3011acf",
+    "url": "/babyTracker/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3e95a3bb1b9de31c5851",
+    "url": "/babyTracker/static/js/2.e873d607.chunk.js"
+  },
+  {
+    "revision": "a3fb8a051b154c089c5dba8c6d16a92d",
+    "url": "/babyTracker/static/js/2.e873d607.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec61c85e423fe3011acf",
+    "url": "/babyTracker/static/js/main.c3c79eb0.chunk.js"
+  },
+  {
+    "revision": "d9c71700a2de10ad40f1",
+    "url": "/babyTracker/static/js/runtime-main.9f0d2acf.js"
+  }
+]);
