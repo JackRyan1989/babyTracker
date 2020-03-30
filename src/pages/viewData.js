9da5bc8dcd5c26dfe82124ebae223ed0073d41cc
JackRyan1989 <jack.jackryan@gmail.com>
2020-03-30 18:42:20 -0400

    Set graph widths to max.

--- src/pages/viewData.js
+++ src/pages/viewData.js
@@ -10,9 +10,9 @@ export default function ViewData(props) {
         <>
         <HeaderHideScroll {...props}></HeaderHideScroll>
         <Grid container spacing={2}>
-            <Grid item xs={12} sm={12} md={6} lg={6}><DisplayBurden {...props}/></Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6}><SleepGraph {...props}/></Grid>
-            <Grid item xs={12} sm={12} md={6} lg={6}><SleepWakeGraph {...props}/></Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}><DisplayBurden {...props}/></Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}><SleepGraph {...props}/></Grid>
+            <Grid item xs={12} sm={12} md={12} lg={12}><SleepWakeGraph {...props}/></Grid>
         </Grid>
         </>
     )
