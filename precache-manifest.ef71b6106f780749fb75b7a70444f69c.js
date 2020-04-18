108e0978e6e85a303ef061e8a97f50eebfb78190
JackRyan1989 <jack.jackryan@gmail.com>
2020-04-17 21:27:30 -0400

    Updates

--- precache-manifest.ef71b6106f780749fb75b7a70444f69c.js
+++ precache-manifest.ef71b6106f780749fb75b7a70444f69c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "aeb5780a5bd2ceaa4204281d6ab97723",
+    "revision": "d4ebdf9dadadca6684c95b3f55303035",
     "url": "/babyTracker/index.html"
   },
   {
-    "revision": "662bdb00ec928c3a6607",
+    "revision": "77978b71d949d80ce96c",
     "url": "/babyTracker/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "b15b053c57d52c190057",
-    "url": "/babyTracker/static/js/2.5c0ec8a1.chunk.js"
+    "revision": "80a620604a9c73fa65df",
+    "url": "/babyTracker/static/js/2.ae348640.chunk.js"
   },
   {
     "revision": "a3fb8a051b154c089c5dba8c6d16a92d",
-    "url": "/babyTracker/static/js/2.5c0ec8a1.chunk.js.LICENSE.txt"
+    "url": "/babyTracker/static/js/2.ae348640.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "662bdb00ec928c3a6607",
-    "url": "/babyTracker/static/js/main.0e80dc8a.chunk.js"
+    "revision": "77978b71d949d80ce96c",
+    "url": "/babyTracker/static/js/main.7d27e967.chunk.js"
   },
   {
     "revision": "d9c71700a2de10ad40f1",
